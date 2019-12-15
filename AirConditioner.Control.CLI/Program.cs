4294908ed1f5ad2e0fa63f335ac8271f393f0684
Adriano Vega Llobell <adrianovega@hotmail.com>
2019-12-15 01:01:52 +0100

    Replaced the Windows form project by a CLI control application

---
+++ AirConditioner.Control.CLI/Program.cs
@@ -0,0 +1,27 @@
+﻿using Dim.AirConditioner.Logic;
+using Dim.AirConditioner.Logic.Fakes;
+using System;
+using System.Threading.Tasks;
+
+namespace Dim.AirConditioner.Control.Cli
+{
+    internal static class Program
+    {
+        public static async Task Main()
+        {
+            IAirConditioner airConditioner = new FakeAirConditioner(10);
+            airConditioner.PowerOn();
+
+            while (true)
+            {
+                await airConditioner.StartCoolingMode(5);
+
+                await Task.Delay(TimeSpan.FromSeconds(15));
+
+                await airConditioner.StartHeatingMode(10);
+
+                await Task.Delay(TimeSpan.FromSeconds(10));
+            }
+        }
+    }
+}
